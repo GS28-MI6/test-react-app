782ceb4170e9d0895095561a7308142f67fc85c5
Gabriel Alessandro Sosa <gasosa2811@gmail.com>
2024-11-16 15:07:58 -0300

    image transition animation

--- src/App.js
+++ src/App.js
@@ -15,65 +15,12 @@ import { ReactComponent as Hexagons } from "./assets/hexagonsTest3.svg";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Hello } from "./pages/Hello";
-import giftCard1 from "./assets/giftCard1.png";
-import giftCard2 from "./assets/giftCard2.png";
-import giftCard3 from "./assets/giftCard3.png";
 
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 /* receives array of jsx elements I want to sort them by their id */
 
-const giftCardArray = [
-  giftCard1,
-  giftCard2,
-  giftCard3,
-  giftCard1,
-  giftCard2,
-  giftCard3,
-];
-
-const orderArrayById = (arr) => {
-  return arr.sort((a, b) => {
-    const aId = Number(a.id.replace("hexGroup", "")); // remove #hexGroup and leave number
-    const bId = Number(b.id.replace("hexGroup", ""));
-    console.log(aId, bId);
-    if (aId < bId) {
-      return -1;
-    }
-    if (aId > bId) {
-      return 1;
-    }
-    return 0;
-  });
-};
-
 function App() {
-  useGSAP(() => {
-    gsap.to(".navbar", {
-      scrollTrigger: {
-        trigger: ".navbar",
-        start: "top top",
-        markers: true,
-        onEnter: () => {
-          console.log("onEnter");
-          gsap.set(".navbar", { display: "none" });
-          gsap.fromTo(
-            ".navbarMobile",
-            { display: "flex", yPercent: -100 },
-            { yPercent: 0 }
-          );
-        },
-        onLeaveBack: () => {
-          console.log("onLeaveBack");
-          gsap
-            .timeline({})
-            .to(".navbarMobile", { yPercent: -100 })
-            .to(".navbar", { display: "flex" });
-        },
-      },
-    });
-  }, []);
-
   return (
     <div
       style={{
@@ -83,26 +30,6 @@ function App() {
         overflow: "hidden",
       }}
     >
-      <div
-        style={{
-          display: "flex",
-          width: "100vw",
-          height: "50px",
-          backgroundColor: "red",
-        }}
-        className="navbar"
-      />
-      <div
-        style={{
-          display: "none",
-          width: "100vw",
-          height: "50px",
-          backgroundColor: "blue",
-          position: "fixed",
-          top: "0",
-        }}
-        className="navbarMobile"
-      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hello" element={<Hello />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,63 +7,72 @@ import giftCard1 from "../assets/Giftcard1.png";
 import giftCard2 from "../assets/Giftcard2.png";
 import giftCard3 from "../assets/Giftcard3.png";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import Flip from "gsap/Flip";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { horizontalLoop } from "../helpers/horizontal-loop";
 import { horizontalLoopAlter } from "../helpers/horizontal-loop-alter";
+import image1 from "../assets/Flip/butterfly1.jpeg";
+import image2 from "../assets/Flip/parrot1.jpg";
+import image3 from "../assets/Flip/heron.jpeg";
+import image4 from "../assets/Flip/butterfly2.jpg";
+import image5 from "../assets/Flip/eagel1.jpg";
+import image6 from "../assets/Flip/parrot2.jpg";
 
-gsap.registerPlugin(ScrollTrigger, useGSAP);
+gsap.registerPlugin(ScrollTrigger, useGSAP, Flip);
 
 /* receives array of jsx elements I want to sort them by their id */
 
-const giftCardArray = [
-  giftCard1,
-  giftCard2,
-  giftCard3,
-  giftCard1,
-  giftCard2,
-  giftCard3,
-  giftCard1,
-];
+const imageArray = [image1, image2, image3, image4, image5];
 
 export const Home = () => {
   const imageContainerRef = useRef(null);
-  const activeElement = useRef(null);
-  const loopRef = useRef();
-  const currentImageIndex = useRef(3);
-  const [currentImageIndex2, setCurrentImageIndex2] = useState(3);
 
-  useGSAP(() => {
-    gsap.context(() => {
-      const images = gsap.utils.toArray(".gift-card");
-      console.log(currentImageIndex2);
-      loopRef.current = horizontalLoopAlter(images, {
-        paused: true,
-        center: true,
-        paddingRight: 80,
-        onChange: (element, index) => {
-          // when the active element changes, this function gets called.
-          activeElement.current &&
-            activeElement.current.classList.remove("active");
-          element.classList.add("active");
-          activeElement.current = element;
-          gsap
-            .timeline({})
-            .to(".gift-card", { scale: 0.7, opacity: 0.5, duration: 0.5 })
-            .to(".active", { scale: 1, opacity: 1, duration: 0.5 }, "<");
+  const swapImages = (id) => {
+    if (id === 0) return;
+    const imagesGrid = gsap.utils.toArray(".image-carousel");
+    let divWithImage = document.createElement("div");
+    divWithImage.className = "image-new";
+    divWithImage.style.cssText = `background-image: url(${imageArray[id]});`;
+
+    let fistDivWithImage = document.createElement("div");
+    fistDivWithImage.className = "image-new";
+    fistDivWithImage.style.cssText = `background-image: url(${imageArray[0]});`;
+
+    imagesGrid[0].prepend(divWithImage);
+    imagesGrid[id].prepend(fistDivWithImage);
+    gsap
+      .timeline({
+        onComplete: () => {
+          console.log("list");
+          const currentImage = imagesGrid[id].querySelector(".image");
+          const fistImage = imagesGrid[0].querySelector(".image");
+          gsap.set(currentImage, {
+            backgroundImage: `url(${imageArray[0]})`,
+            opacity: 1,
+          });
+          gsap.set(fistImage, {
+            backgroundImage: `url(${imageArray[id]})`,
+            opacity: 1,
+          });
+          imagesGrid[0].removeChild(divWithImage);
+          imagesGrid[id].removeChild(fistDivWithImage);
+          [imageArray[0], imageArray[id]] = [imageArray[id], imageArray[0]];
         },
-      });
-    }, imageContainerRef.current);
-  });
-
-  const moveImage = (idx) => {
-    currentImageIndex.current = idx;
-    setCurrentImageIndex2(idx);
-    console.log(currentImageIndex2);
-    loopRef.current.toIndex(idx, {
-      duration: 0.5,
-      ease: "power3",
-    });
+      })
+      .to(imagesGrid[0].querySelector(".image"), {
+        opacity: 0,
+        duration: 1.5,
+      })
+      .to(
+        imagesGrid[id].querySelector(".image"),
+        {
+          opacity: 0,
+          duration: 1.5,
+        },
+        "<"
+      );
+      
   };
 
   return (
@@ -71,7 +80,7 @@ export const Home = () => {
       <div
         className="body"
         style={{
-          height: "150vh",
+          height: "100vh",
           width: "100%",
           backgroundColor: "white",
           display: "flex",
@@ -80,37 +89,38 @@ export const Home = () => {
           flexDirection: "column",
         }}
       >
-        <p style={{ fontSize: "5rem", color: "black" }}>Bienvenidos</p>
-        <div
-          ref={imageContainerRef}
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            width: "75vw",
-            overflow: "hidden",
-            alignItems: "center",
-            position: "relative",
-          }}
-        >
-          {giftCardArray.map((giftCard, idx) => (
-            <img
-              src={giftCard}
-              className={"gift-card"}
-              alt=""
-              onClick={() => moveImage(idx)}
-              style={{
-                display: "flex",
-                height: "100%",
-                width: "500px",
-                margin: 0,
-                padding: 0,
-                position: "relative",
-                flexShrink: 0,
-                fontSize: "21px",
-                cursor: "pointer",
-                marginRight: 80,
-              }}
-            />
+        <div ref={imageContainerRef} className="image-carousel-container">
+          {imageArray.map((image, idx) => (
+            <div className="image-carousel">
+              <div
+                key={idx}
+                className="image"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  backgroundImage: `url(${image})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  zIndex: 10,
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                }}
+              />
+              <div
+                className="overlay"
+                onClick={() => swapImages(idx)}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: "0",
+                  left: "0",
+                  backgroundColor: "rgb(255 255 255 / 0%)",
+                  zIndex: 11,
+                }}
+              />
+            </div>
           ))}
         </div>
       </div>
